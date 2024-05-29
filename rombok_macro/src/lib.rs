c4921d08e3aeb3c55127830a704f6cc1b82ed93a
myyrakle <sssang97@naver.com>
2024-05-30 00:54:08 +0900

    [#10] EqualsAndHashcode 구현

--- rombok_macro/src/lib.rs
+++ rombok_macro/src/lib.rs
@@ -217,6 +217,38 @@ pub fn NoArgsConstructor(_: TokenStream, mut item: TokenStream) -> TokenStream {
     return item;
 }
 
+#[proc_macro_attribute]
+#[allow(non_snake_case)]
+pub fn EqualsAndHashcode(_: TokenStream, mut item: TokenStream) -> TokenStream {
+    let struct_info = extract_struct_info(item.clone());
+
+    if !struct_info.is_struct {
+        panic!("The #[EqualsAndHashcode] attribute can only be used on structs");
+    }
+
+    let mut new_code = String::new();
+
+    new_code += &format!("impl PartialEq for {} {{\n", struct_info.struct_name);
+    new_code += &format!("  fn eq(&self, other: &Self) -> bool {{\n");
+    new_code += &format!("      true\n");
+    new_code += &format!("  }}\n");
+    new_code += &format!("}}\n");
+
+    new_code += &format!("impl Eq for {} {{}}\n", struct_info.struct_name);
+
+    new_code += &format!("impl Eq for {} {{}}\n", struct_info.struct_name);
+
+    new_code += &format!("impl std::hash::Hash for {} {{\n", struct_info.struct_name);
+    new_code += &format!("  fn hash<H: std::hash::Hasher>(&self, state: &mut H) {{\n");
+    new_code += &format!("      self.0.hash(state);\n");
+    new_code += &format!("  }}\n");
+    new_code += &format!("}}\n");
+
+    item.extend(TokenStream::from_str(&new_code).unwrap());
+
+    return item;
+}
+
 #[derive(Debug)]
 struct StructInfo {
     struct_name: String,
